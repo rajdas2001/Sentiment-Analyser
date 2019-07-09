16e18e43b3294ea5f755a2ef71c85f0b2bd1bbc4
Raj Das <rajdas.2001@icloud.com>
2019-07-09 19:03:20 +0530

    Delete sentinalgit.py

--- sentinalgit.py
+++
@@ -1,9 +0,0 @@
-from aylienapiclient import textapi
-
-#application_id = ""
-#application_key = ""
-x = input("Enter a sentence")
-c = textapi.Client("Enter application id", "Enter application key")
-s = c.Sentiment({'text': x})
-print(s)
-print(s['polarity'])
